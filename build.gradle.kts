7d287dc5241db425cbddbb09b9fb41dc6aeda613
Peter Vestlin <peter.vestlin@lm.se>
2019-11-02 17:37:05 +0100

    Tests for UserHandlerTest.

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,14 +30,18 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
     developmentOnly("org.springframework.boot:spring-boot-devtools")
+
     runtimeOnly("com.h2database:h2")
     runtimeOnly("io.r2dbc:r2dbc-h2")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
     testImplementation("org.springframework.boot.experimental:spring-boot-test-autoconfigure-r2dbc")
     testImplementation("io.projectreactor:reactor-test")
+    testImplementation("io.mockk:mockk:1.9.3")
 }
 
 dependencyManagement {
---
+++ src/test/kotlin/nu/westlin/r2dbcdemo/UserHandlerTest.kt
@@ -0,0 +1,162 @@
+package nu.westlin.r2dbcdemo
+
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.runBlocking
+import nu.westlin.r2dbcdemo.UserRepository.CreateResult
+import nu.westlin.r2dbcdemo.UserRepository.UpdateResult
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.test.web.reactive.server.expectBody
+import org.springframework.test.web.reactive.server.expectBodyList
+
+internal class UserHandlerTest {
+
+    private val userRepository = mockk<UserRepository>()
+    private val handler = UserHandler(userRepository)
+
+    private lateinit var client: WebTestClient
+
+    private val user1 = User(1, "User 1")
+    private val user2 = User(2, "User 2")
+    private val user3 = User(3, "User 3")
+
+    @Suppress("unused")
+    @BeforeAll
+    private fun init() {
+        client = WebTestClient.bindToRouterFunction(WebConfiguration().routes(handler)).build()
+    }
+
+    @Test
+    fun `all users`() {
+        runBlocking {
+            coEvery { userRepository.all() } returns listOf(user1, user2).asFlow()
+
+            client.get().uri("/users").accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBodyList<User>().contains(user1, user2)
+        }
+    }
+
+    @Test
+    fun `a user by id`() {
+        runBlocking {
+            val user = user3
+            coEvery { userRepository.byId(user.id) } returns user3
+
+            client.get().uri("/users/{id}", user.id).accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody<User>().isEqualTo(user)
+        }
+    }
+
+    @Test
+    fun `a user by id that does not exist`() {
+        runBlocking {
+            val user = user3
+            coEvery { userRepository.byId(user.id) } returns null
+
+            client.get().uri("/users/{id}", user.id).accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isNotFound
+                .expectHeader().doesNotExist("Accept")
+                .expectBody().isEmpty
+        }
+    }
+
+    @Test
+    fun `create a user`() {
+        runBlocking {
+            val user = User(4, "User 4")
+            coEvery { userRepository.create(user) } returns CreateResult.CREATED
+
+            client.post().uri("/users").contentType(MediaType.APPLICATION_JSON).bodyValue(user)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isCreated
+                .expectHeader().doesNotExist("Accept")
+                .expectBody().isEmpty
+        }
+    }
+
+    @Test
+    fun `create a user that already exist`() {
+        runBlocking {
+            val user = User(4, "User 4")
+            coEvery { userRepository.create(user) } returns CreateResult.ALREADY_EXIST
+
+            client.post().uri("/users").contentType(MediaType.APPLICATION_JSON).bodyValue(user)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.CONFLICT)
+                .expectHeader().doesNotExist("Accept")
+                .expectBody().isEmpty
+        }
+    }
+
+    @Test
+    fun `update a user`() {
+        runBlocking {
+            val user = User(4, "User 4")
+            coEvery { userRepository.update(user) } returns UpdateResult.UPDATED
+
+            client.put().uri("/users").contentType(MediaType.APPLICATION_JSON).bodyValue(user)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isNoContent
+                .expectHeader().doesNotExist("Accept")
+                .expectBody().isEmpty
+        }
+    }
+
+    @Test
+    fun `update a user that does not exist`() {
+        runBlocking {
+            val user = User(4, "User 4")
+            coEvery { userRepository.update(user) } returns UpdateResult.NOT_FOUND
+
+            client.put().uri("/users").contentType(MediaType.APPLICATION_JSON).bodyValue(user)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isNotFound
+                .expectHeader().doesNotExist("Accept")
+                .expectBody().isEmpty
+        }
+    }
+
+    @Test
+    fun `delete a user`() {
+        runBlocking {
+            val user = user3
+            coEvery { userRepository.delete(user.id) } returns UserRepository.DeleteResult.DELETED
+
+            client.delete().uri("/users/{id}", user.id).accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk
+                .expectHeader().doesNotExist("Accept")
+                .expectBody().isEmpty
+        }
+    }
+
+    @Test
+    fun `delete a user that does not exist`() {
+        runBlocking {
+            val user = user3
+            coEvery { userRepository.delete(user.id) } returns UserRepository.DeleteResult.NOT_FOUND
+
+            client.delete().uri("/users/{id}", user.id).accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isNotFound
+                .expectHeader().doesNotExist("Accept")
+                .expectBody().isEmpty
+        }
+    }
+}
